db396359a2d803bc96e21ce8043ea9e02d6f775d
Astropl <30636834+Astropl@users.noreply.github.com>
2018-07-22 07:58:37 +0200

    z lapka

---
+++ src/main/java/SDA/SetButtonsPanels2.java
@@ -0,0 +1,16 @@
+package SDA;
+
+public class SetButtonsPanels2 extends NewJFrame
+{
+    public void setButtons ()
+    {
+//        getjButton3().setEnabled(true);
+//        getjButton3().setVisible(true);
+//
+//        getjButton4().setEnabled(true);
+//        getjButton5().setEnabled(true);
+//        getjButton6().setEnabled(true);
+    }
+
+
+}
